7e9457251f1bd192006c2aee61c8f263484367f7
Matias Forbord <codeluggage@posteo.net>
2022-03-25 12:30:29 +0800

    experimenting with build vs compile

--- imba-vitest/app/app.imba
+++ imba-vitest/app/app.imba
@@ -3,17 +3,6 @@ def answer(input\number)
 	return true if input === -42
 	false
 
-global css html
-	ff:sans
-
-tag app
-	<self>
-		<header>
-			<svg[w:200px h:auto] src='./logo.svg'>
-			<p> "Edit {<code> "app/client.imba"} and save to reload"
-			<a href="https://imba.io"> "Learn Imba"
-
-imba.mount <app>
 
 export {
 	answer
---
+++ imba-vitest/test/app/app.js
@@ -0,0 +1,4 @@
+//__HEAD__
+var n=Object.defineProperty;var f=r=>n(r,"__esModule",{value:!0});var a=(r,e)=>{for(var t in e)n(r,t,{get:e[t],enumerable:!0})};f(exports);a(exports,{answer:()=>u});function u(r){return r===42||r===-42}0&&(module.exports={answer});
+//__FOOT__
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiYXBwLmltYmEiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImRlZiBhbnN3ZXIoaW5wdXRcXG51bWJlcilcblx0cmV0dXJuIHRydWUgaWYgaW5wdXQgPT09IDQyXG5cdHJldHVybiB0cnVlIGlmIGlucHV0ID09PSAtNDJcblx0ZmFsc2VcblxuXG5leHBvcnQge1xuXHRhbnN3ZXJcbn1cbiJdLAogICJtYXBwaW5ncyI6ICI7cUtBQUEsV0FBVyxFQUFhLENBRVgsTUFERyxLQUFVLElBQ1YsSUFBVSIsCiAgIm5hbWVzIjogW10KfQo=
